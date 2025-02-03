d26acaba69bda4aee4c5a6b66442f1fdca7761e3
Vinicius Lenartevitz <viniciuslenartevitz@gmail.com>
2025-02-03 19:49:13 -0300

    Inicio do projeto, instalando react e o keyclockify, e começando a tela de login

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode, Suspense } from "react";
+import { KcContext, KcPage } from "./keycloak-theme/kc.gen";
+import App from "./App";
+
+// The following block can be uncommented to test a specific page with `yarn dev`
+// Don't forget to comment back or your bundle size will increase
+/*
+import { getKcContextMock } from "./login/KcPageStory";
+
+if (import.meta.env.DEV) {
+    window.kcContext = getKcContextMock({
+        pageId: "register.ftl",
+        overrides: {}
+    });
+}
+*/
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        {window.kcContext ? (
+            <KcPage kcContext={window.kcContext} />
+        ) : (
+            <Suspense>
+                <App />
+            </Suspense>
+        )}
+    </StrictMode>
+);
+
+declare global {
+    interface Window {
+        kcContext?: KcContext;
+    }
+}
